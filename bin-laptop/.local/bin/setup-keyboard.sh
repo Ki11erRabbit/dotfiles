db1e3ce8a08ab528a0360e0cae7fc05587efe2a9
Alec Davis <unlikelytitan@gmail.com>
2024-03-18 18:28:56 -0600

    added kmonad layout switching

--- bin-laptop/.local/bin/setup-keyboard.sh
+++ bin-laptop/.local/bin/setup-keyboard.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+model=$(sudo /run/current-system/sw/bin/dmidecode | grep Version | head -n 2 | tail -n 1 | awk '{print $3}')
 
 
-riverctl keyboard-layout -model pc104 -variant colemak_dh -options "grp:ctl_space_toggle" us
+case $model in
+    T440p) kmonad /home/ki11errabbit/.config/kmonad/t44p.kbd ;;
+esac
+
+#riverctl keyboard-layout -model pc104 -variant colemak_dh -options "grp:ctl_space_toggle" us
--- nixos/.config/home-manager/home.nix
+++ nixos/.config/home-manager/home.nix
@@ -20,6 +20,7 @@
   home.packages = with pkgs; [
     stow
     fnott
+    wdisplays
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello

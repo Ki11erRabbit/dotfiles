6492ebee07c14f1f77ef39ab9ff4187f5e5cc964
ki11errabbit <ki11errabbit@posteo.net>
2024-07-06 18:51:02 -0700

    added kakoune configs

---
+++ setup-kakoune.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mkdir -p $HOME/.config/kak/autoload/
+mkdir -p $HOME/.config/kak/bundle/
+git clone https://codeberg.org/jdugan6240/kak-bundle $HOME/.config/kak/bundle/kak-bundle
+
+stow kakoune
+#mkdir -p "$HOME/kak/autoload"
+ln -s "$(nix-store -r $(which kak) 2> /dev/null)/share/kak/autoload" "$HOME/.config/kak/autoload/default"
